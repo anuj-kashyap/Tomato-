ea56e21462f04693322d7230858bebaa52037392
anuj-kashyap <kashyapanuj713@gmail.com>
2024-06-21 17:53:11 +0530

    changes

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -6,8 +6,13 @@ const AppDownload = () => {
     <div className='app-download' id='app-download'>
         <p>For better expreince Download <br/> Tomato App </p>
         <div className="app-download-platorm">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+          <a href="https://play.google.com/store/games?hl=en&pli=1" target="_blank" rel="noopener noreferrer">
+          <img src={assets.play_store} alt="" />
+          </a>
+          <a href="https://play.google.com/store/games?hl=en&pli=1" target="_blank" rel="noopener noreferrer">
+          
+             <img src={assets.app_store} alt="" />
+             </a>
         </div>
 
     </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.css'
 import { assets } from '../../assets/assets'
 
 const Footer = () => {
+    const [menu, setMenu] = useState("Home")
   return (
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
                 <img src={assets.logo} alt="" />
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellat veritatis iure, enim impedit eos consequuntur quo id tempore libero!</p>
-                <div className="footer-social-icon">
-                    <img src={assets.facebook_icon} alt="" />
+                {/* <div className="footer-social-icon">
+                    <img src={assets.facebook_icon}  alt="" />
                     <img src={assets.twitter_icon} alt="" />
                     <img src={assets.linkedin_icon} alt="" />
+                </div> */}
+                 <div className="footer-social-icon">
+                    <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <img src={assets.facebook_icon} alt="Facebook" />
+                    </a>
+                    <a href="https://www.twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <img src={assets.twitter_icon} alt="Twitter" />
+                    </a>
+                    <a href="https://www.linkedin.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <img src={assets.linkedin_icon} alt="LinkedIn" />
+                    </a>
                 </div>
             </div>
-            <div className="footer-content-centre">
-                <h2>COMPANY</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
-                </ul>
-                
-            </div>
+         
             <div className="footer-content-right">
                 <h2>GET IN TOUCH</h2>
                 <ul>
